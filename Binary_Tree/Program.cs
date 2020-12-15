9ea318282d1f2988080233eaf32bfa2d210eecac
Leonid Plisov <lekas628@gmail.com>
2020-12-15 15:50:28 +0300

    fix and improve tree rotation

--- Binary_Tree/Program.cs
+++ Binary_Tree/Program.cs
@@ -10,43 +10,105 @@ namespace Program
 
         static void Main(string[] args)
         {
+            //List<int> output = new List<int>();
+            //List<int> input = new List<int>() { 3,3,4,3,0};
+            //Node root = null;
+
+            //foreach (int element in input)
+            //    root = Tree.Tree.Insert(root, element);
+
+            ////root = new Node(0);
+            ////root.leftChild = new Node(0);
+            ////root.leftChild.leftChild = new Node(0);
+            ////root.leftChild.rightBrother = new Node(4);
+            ////root.leftChild.rightBrother.leftChild = new Node(4);
+
+
+            ////root = Tree.Tree.Remove(root, 4);
+            //int maxDeep = Tree.Tree.printTree(root);
+
+            //Tree.Tree.InorderTraversal(root, output);
+
+            //input.Remove(4);
+            //CheckTree(input, output);
+
+            for(int i = 0; i < 10; i++)
+                run();
+            
+            Console.ReadKey();
+        }
+
+        static void CheckTree(List<int> input, List<int> treeOutput)
+        {
+            bool result = true;
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                if (input[i] != treeOutput[i])
+                {
+                    result = false;
+                }
+            }
+            if (input.Count != treeOutput.Count)
+                result = false;
+            Console.WriteLine(result);
+        }
+
+        static void run()
+        {
+
             Node root = null;
 
 
-            bool printOutput = true;
+            bool printOutput = false;
 
-            int n = 5;
+            int n = 100;
             int maxNumber = n;
             int maxElements = n;
 
             List<int> inputListSorted = new List<int>();
             List<int> CheckList = new List<int>();
 
-
-            Console.WriteLine("INPUT");
+            if(printOutput)
+                Console.WriteLine("INPUT");
             for (int i = 0; i < maxElements; i++)
             {
                 int element = rand.Next() % maxNumber;
+                inputListSorted.Add(element);
+                if (printOutput)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine($"ADD ELEMENT {element}");
+                    Console.Write("must be in list ");
+                    foreach (int elem in inputListSorted)
+                        Console.Write($"{elem} ");
+                    Console.WriteLine("\n");
+                }
 
                 root = Tree.Tree.Insert(root, element);
-                inputListSorted.Add(element);
-                if(printOutput)
-                    Console.WriteLine(element);
             }
             inputListSorted.Sort();
 
-
-            Console.WriteLine("SORTED INPUT LIST");
+            if(printOutput)
+                Console.WriteLine("SORTED INPUT LIST");
             foreach (int element in inputListSorted)
-            { 
+            {
                 if (printOutput)
                     Console.WriteLine(element);
             }
 
-
-            Console.WriteLine("TREE PRINT AFTER FILL");
+            if(printOutput)
+                Console.WriteLine("TREE PRINT AFTER FILL");
             int maxDeep = Tree.Tree.printTree(root, onlyGetMaxDeep: !printOutput);
             Console.WriteLine("\n");
+
+
+
+
+            //Console.WriteLine($"REMOVE ELEMENT {inputListSorted[3]}");
+            //Tree.Tree.Remove(root, inputListSorted[3]);
+            //Tree.Tree.printTree(root);
+            //inputListSorted.Remove(inputListSorted[3]);
+
 
 
             CheckList.Clear();
@@ -54,16 +116,23 @@ namespace Program
 
 
             bool result = true;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < inputListSorted.Count - 1; i++)
             {
                 if (inputListSorted[i] != CheckList[i])
+                {
                     result = false;
+                }
             }
-            Console.WriteLine(result);
+            if (inputListSorted.Count != CheckList.Count)
+                result = false;
+            
+            Console.WriteLine($"RESULT\t\t{result}");
 
-            Console.WriteLine($"\n\nMAX DEEP IS {maxDeep}");
-            Console.ReadKey();
-
+            Console.WriteLine($"\nMAX DEEP IS {maxDeep}");
+            Console.WriteLine($"ROOT SIZE IS {root.size}");
+            if (printOutput)
+                Console.ReadKey();
         }
     }
+
 }
--- Binary_Tree/tree.cs
+++ Binary_Tree/tree.cs
@@ -7,6 +7,12 @@ namespace Tree
     {
 
         public static Random rand = new Random();
+        static public bool debug = false;
+        static public bool debug_1 = false; // дебаг поворотов
+        static public bool debug_2 = false; // отрисовка деревьев во время дебага
+        static public int debugTmp1;
+        static public int debugTmp2;
+        
 
         public static Node Find(Node node, int value)
         {
@@ -37,7 +43,7 @@ namespace Tree
             }
             else if (rand.Next() % (node.size + 1) == 0)
             {
-                return InsertRoot(node, value);
+                node = InsertRoot(node, value);
             }
             else if (value <= node.value)
             {
@@ -51,6 +57,15 @@ namespace Tree
                 }
                 node.leftChild.rightBrother = Insert(node.leftChild.rightBrother, value);
             }
+            fixSize(node);
+
+            if(debug_2)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine($"PRINT TREE AFTER INSERT {value}");
+                Tree.printTree(node);
+            }
+
             return node;
         }
 
@@ -79,65 +94,274 @@ namespace Tree
             }
         }
 
-
         public static Node rotateRight(Node p)
         {
-            Node q = p.leftChild;
-            if (q == null || q.mark == 1)
+            if (p == null)
                 return p;
 
+            Node q = p.leftChild;
+
+            if (q == null)
+                return p;
+            if (q.mark == 1)
+                return p;
+            if (p.mark == 1)
+                return p;
+
+            if(debug_2)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("rotate right print tree BEFORE rotate");
+                Tree.printTree(p);
+            }
+
+            if(debug_1)
+            {
+                debugTmp1 = p.leftChild.size;
+                debugTmp2 = 0;
+                if(p.leftChild.rightBrother != null)
+                    debugTmp2 = p.leftChild.rightBrother.size;
+            }
+
+
             Node pRightBrother = p.rightBrother;
-            if(q.leftChild == null)
-            {
-                q.leftChild = new Node(-1, mark: 1);
-            }
-            if(q.leftChild.rightBrother == null)
-            {
-                q.leftChild.rightBrother = new Node(-1, mark: 1);
-            }
-            
-            p.leftChild = q.leftChild.rightBrother;
-            p.leftChild.rightBrother = q.rightBrother;
             p.rightBrother = null;
 
-            q.rightBrother = pRightBrother;
+            if (q.leftChild != null)
+            {
+                p.leftChild = q.leftChild.rightBrother;
+                q.leftChild.rightBrother = null;
+            }
+            else
+            {
+                p.leftChild = null;
+                q.leftChild = new Node(-1, 1);
+            }
+
+            if (q.rightBrother != null)
+            {
+                if (p.leftChild == null)
+                    p.leftChild = new Node(-1, 1);
+                p.leftChild.rightBrother = q.rightBrother;
+                q.rightBrother = null;
+            }
+
             q.leftChild.rightBrother = p;
+            q.rightBrother = pRightBrother;
 
             fixSize(p);
             fixSize(q);
 
+            if(debug_1)
+                Console.WriteLine($"Rotate right left({debugTmp1}) right({debugTmp2}) sum({debugTmp1 + debugTmp2})");
+            if (debug_2)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("rotate right print tree AFTER rotate");
+                Tree.printTree(p);
+            }
+
+
             return q;
         }
+
+
 
         public static Node rotateLeft(Node q)
         {
             if (q.leftChild == null)
-                return q;
+                return null;
             if (q.leftChild.rightBrother == null)
-                return q;
+                return null;
 
             Node p = q.leftChild.rightBrother;
+
+            if(debug_1)
+            {
+                debugTmp1 = q.leftChild.size;
+                debugTmp2 = q.leftChild.rightBrother.size;
+            }
+
+            if (debug_2)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("rotate left print tree BEFORE rotate");
+                Tree.printTree(p);
+            }
+
             p.rightBrother = q.rightBrother;
-            
+            q.rightBrother = null;
             if (p.leftChild != null)
             {
-                q.rightBrother = p.leftChild.rightBrother;
-                p.leftChild.rightBrother = null;
+                if (p.leftChild.rightBrother != null)
+                {
+                    q.rightBrother = p.leftChild.rightBrother;
+                    p.leftChild.rightBrother = null;
+
+                    if(p.leftChild.mark == 1)
+                    {
+                        q.leftChild.rightBrother = p.leftChild;
+                        p.leftChild = null;
+                    }
+
+                }
                 q.leftChild.rightBrother = p.leftChild;
+                p.leftChild = null;
             }
             else
             {
-                q.rightBrother = null;
                 q.leftChild.rightBrother = null;
+                if (q.leftChild.mark == 1)
+                    q.leftChild = null;
             }
-            
+
             p.leftChild = q;
 
             fixSize(q);
             fixSize(p);
 
+            if(debug_1)
+                Console.WriteLine($"Rotate left left({debugTmp1}) right({debugTmp2}) sum({debugTmp1 + debugTmp2})");
+            if (debug_2)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("rotate left print tree AFTER rotate");
+                Tree.printTree(p);
+            }
+
+
             return p;
+
         }
+
+  
+        //public static Node Join(Node p, Node q)
+        //{
+        //    if (p == null)
+        //        return q;
+        //    if (q == null)
+        //        return p;
+        //    if (p.rightBrother == q)
+        //        p.rightBrother = null;
+
+        //    if (rand.Next() % (p.size + q.size) < p.size)
+        //    {
+        //        if(p.leftChild == null)
+        //            p.leftChild = new Node(-1, mark: 1);
+        //        p.leftChild.rightBrother = Join(p.leftChild.rightBrother, q);
+        //        fixSize(p);
+        //        return p;
+        //    }
+        //    else
+        //    {
+        //        if(q.leftChild != null)
+        //        {
+        //            if(q.leftChild.mark == 1)
+        //            {
+        //                q.leftChild.value = p.value;
+        //                q.leftChild = p.leftChild;
+        //                q.mark = 0;
+        //                fixSize(q);
+        //                return q;
+        //            }
+
+        //        }
+        //        q.leftChild = Join(p, q.leftChild);
+
+
+        //        fixSize(p);
+        //        fixSize(q);
+
+        //        return q;
+        //    }
+        //}
+
+        //public static Node Remove(Node node, int value)
+        //{
+        //    if (node == null)
+        //        return node;
+        //    if (node.mark == 1)
+        //        return node;
+
+        //    if (node.value == value)
+        //    {
+        //        if (node.leftChild != null)
+        //        {
+        //            if(node.leftChild.mark == 1)
+        //            {
+        //                node.leftChild.rightBrother.rightBrother = node.rightBrother;
+        //                node = node.leftChild.rightBrother;
+        //                return node;
+        //            }
+        //            if(node.leftChild.rightBrother == null)
+        //            {
+        //                node.leftChild.rightBrother = node.rightBrother;
+        //                node = node.leftChild;
+        //                return node;
+        //            }
+        //            //Node tmp = Join(node.leftChild, node.leftChild.rightBrother);
+        //            //node = null;
+        //            node = Join(node.leftChild, node.leftChild.rightBrother);
+        //            return node;
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //            //else
+        //            //{
+        //            //    node.value = -1;
+        //            //    node.mark = 1;
+        //            //    return node;
+        //            //}
+        //        }
+        //    }
+        //    else if (value < node.value)
+        //    {
+        //        node.leftChild = Remove(node.leftChild, value);
+        //        return node;
+        //    }
+        //    else
+        //    {
+        //        if(node.leftChild != null)
+        //        {
+        //            node.leftChild.rightBrother = Remove(node.leftChild.rightBrother, value);
+        //            return node;
+        //        }
+        //        else
+        //        {
+        //            return node;
+        //        }
+        //    }
+
+        //}
+
+        //node* remove(node* p, int k) // удаление из дерева p первого найденного узла с ключом k 
+        //{
+        //    if (!p) return p;
+        //    if (p->key == k)
+        //    {
+        //        node* q = join(p->left, p->right);
+        //        delete p;
+        //        return q;
+        //    }
+        //    else if (k < p->key)
+        //        p->left = remove(p->left, k);
+        //    else
+        //        p->right = remove(p->right, k);
+        //    return p;
+        //}
+
+        //public static Node Remove(Node node, int value)
+        //{
+        //    if (node == null)
+        //        return null;
+        //    if (node.mark != 0)
+        //        return null;
+
+        //}
+
+
+
 
         static public int printTree(Node node, int prefixNumber = 0, bool isRoot = false, int mark = 0, bool onlyGetMaxDeep = false)
         {
@@ -157,16 +381,17 @@ namespace Tree
                 output += $" VIRTUAL";
             else
                 output +=  $" VALUE({node.value})";
-            
-            
+
+
             //if(isRoot) { output = "ROOT" + output; }
 
             int prefixCounter = 0;
-            while (prefixCounter < prefixNumber)
+            while (prefixCounter <= prefixNumber)
             {
                 output = "+" + output;
                 prefixCounter++;
             }
+
             output = $"{prefixNumber}\t" + output;
             if(!onlyGetMaxDeep)
                 Console.WriteLine(output);
